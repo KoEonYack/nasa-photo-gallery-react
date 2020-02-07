496e9505651afb6586a46ef95041518d9ae04220
KoEonYack <21300035@handong.edu>
2020-02-07 20:33:30 +0900

    Add json search module

---
+++ src/User_test.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Users() {
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+        setError(null);
+        setUsers(null);
+        // loading 상태를 true 로 바꿉니다.
+        setLoading(true);
+        const response = await axios.get(
+          'https://images-api.nasa.gov/search?q=seoul'
+        );
+        setUsers(response.data); // 데이터는 response.data 안에 들어있습니다.
+        console.log(response.data);
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return null;
+  return (
+    <ul>
+        {users.links.map(user => (
+        <li>
+          {user.href} 
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Users;
