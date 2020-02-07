13bf924c1bde4916b12db9c753de6399adde0078
KoEonYack <21300035@handong.edu>
2020-02-07 22:41:27 +0900

    NASA 이미지를 엘범형식으로 불러옴

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './App.css';
-import Users from './components/Users';
+//import Users from './components/Users';
+import Users from './components/new_users';
 import Search from './components/Search'
 import Body from './components/Body'
 
--- src/components/new_users.js
+++ src/components/new_users.js
@@ -46,25 +46,43 @@ class Users extends React.Component {
         if (!this.state.users) return null;
         return (
           <div>
-            <input type="text" value={this.state.text} onChange={this.setText} name="text"/>
-            <button onClick={this.fetchUsers}>검색</button>
-            
-            
-            <ul>
+
+    <main role="main">
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <div className="row">
                 {this.state.users.collection.items.map(user => {
                     if (user.links)
                         return (
-                            <li>
-                                <img src={user.links[0].href}/>
-                                <p> {user.data[0].title} </p>
-                            </li>
+                            <div className="col-md-4">
+                                <div className="card mb-4 box-shadow">
+                                    <img className="card-img-top" src={user.links[0].href} alt="NASA image"/>
+                                    <div className="card-body">
+                                        <p className="card-text">{user.data[0].title}</p>
+                                        <div className="d-flex justify-content-between align-items-center">
+                                        <small className="text-muted">9 mins</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         )
                     else return <p> no-image</p>
                 })}
-            </ul>
-
-
+            </div>
+          </div>
+		</div>
+    </main>
             
+
+
+
+
+
+
+
+		
+
+
           </div>
         );
     }
